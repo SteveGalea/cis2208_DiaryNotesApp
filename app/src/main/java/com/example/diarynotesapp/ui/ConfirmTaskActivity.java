e4b2168939a08ea274357e8d3b82a3c3d67ccca0
Steve Galea <steve.galea.20@um.edu.mt>
2022-05-14 22:51:26 +0200

    added db interaction and confirm activity

---
+++ app/src/main/java/com/example/diarynotesapp/ui/ConfirmTaskActivity.java
@@ -0,0 +1,42 @@
+package com.example.diarynotesapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.diarynotesapp.R;
+import com.example.diarynotesapp.TasksUI.Task;
+import com.example.diarynotesapp.backend.DbHelperTasks;
+
+public class ConfirmTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_confirm);
+        populateScreen();
+    }
+    private void populateScreen() {
+        DbHelperTasks dbHelper = new DbHelperTasks(this);
+        Intent intent = getIntent();
+        long id = intent.getLongExtra("ID", -1); //-1 is default value
+        // gets email from db using id
+        Task task = dbHelper.getTaskById(id);
+        TextView idConfirm = (TextView) findViewById(R.id.id_confirm);
+        idConfirm.setText(task.getId()+"");
+        TextView nameConfirm = (TextView) findViewById(R.id.name_confirm);
+        nameConfirm.setText(task.getName());
+        TextView detailsConfirm = (TextView)
+                findViewById(R.id.details_confirm);
+        detailsConfirm.setText(task.getDetails());
+        TextView dateConfirm = (TextView)
+                findViewById(R.id.date_confirm);
+        dateConfirm.setText(task.getDateDue());
+        TextView sliderConfirm = (TextView)
+                findViewById(R.id.slider_confirm);
+        dateConfirm.setText(task.getProgress());
+    }
+
+}
--- app/src/main/java/com/example/diarynotesapp/ui/TaskActivity.java
+++ app/src/main/java/com/example/diarynotesapp/ui/TaskActivity.java
@@ -1,27 +1,63 @@
 package com.example.diarynotesapp.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.transition.Slide;
+import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.example.diarynotesapp.R;
+import com.example.diarynotesapp.TasksUI.Task;
+import com.example.diarynotesapp.backend.DbHelperTasks;
+import com.example.diarynotesapp.ui.tasks.TasksFragment;
+import com.example.diarynotesapp.ui.tasks.TasksViewModel;
 import com.google.android.material.slider.Slider;
 
 public class TaskActivity extends AppCompatActivity {
 
-
+    Button saveButton;
     DatePicker simpleDatePicker;
     Slider slider;
+    EditText taskName;
+    EditText taskDetails;
+    String sliderValue ="0.0";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+        saveButton = (Button) findViewById(R.id.filledButton);
 
-        //initiate needed data
-        simpleDatePicker = (DatePicker) findViewById(R.id.simpleDatePicker);
+        taskName = (EditText) findViewById(R.id.task_name);
+        taskDetails = (EditText) findViewById(R.id.task_details);
+
+        simpleDatePicker = (DatePicker) findViewById(R.id.date_picker_spinner);
         slider = (Slider) findViewById(R.id.slider);
+
+
+        slider.addOnChangeListener(new Slider.OnChangeListener() {
+            @SuppressLint("RestrictedApi")
+            @Override
+            public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
+                //Use the value
+                sliderValue= ""+value+"";
+
+            }
+        });
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickSave(v);
+            }
+        });
+        //initiate needed data
         /*slider.addOnSliderTouchListener(object : Slider.OnSliderTouchListener {
             override fun onStartTrackingTouch(slider: Slider) {
                 // Responds to when slider's touch event is being started
@@ -35,5 +71,35 @@ public class TaskActivity extends AppCompatActivity {
         slider.addOnChangeListener { slider, value, fromUser ->
             // Responds to when slider's value is changed
         }*/
+    }
+    private void onClickSave(View view){
+
+        DbHelperTasks dbHelperTasks = new DbHelperTasks(this);
+        String dateValue = getDate(simpleDatePicker);
+        String taskNameValue = taskName.getText().toString();
+        String taskDetailsValue = taskDetails.getText().toString();
+
+        Task task = new Task(-1, taskNameValue, taskDetailsValue, sliderValue, dateValue);
+
+        long id = dbHelperTasks.insertTask(task);
+
+        Intent intent = new Intent(this, ConfirmTaskActivity.class);
+        intent.putExtra("ID",id);
+        startActivity(intent);
+    }
+    private String getDate(DatePicker simpleDatePicker){
+        String delimeter = "/";
+        int day = simpleDatePicker.getDayOfMonth();
+        String dayDate = day+delimeter;
+        int month = simpleDatePicker.getMonth();
+        String monthDate = month+delimeter;
+        int year = simpleDatePicker.getYear();
+        if(day < 10){
+            dayDate = "0"+dayDate;
+        }
+        if(month < 10){
+            monthDate = "0"+monthDate;
+        }
+        return dayDate+monthDate+year;
     }
 }
